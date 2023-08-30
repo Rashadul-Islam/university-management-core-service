70d428fd6589d0848c14e13ebc789feb628b5c27
Rashadul-Islam <rashadulislam4581@gmail.com>
2023-08-30 13:43:31 +0600

    [DONE] offered course section module

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -9,6 +9,7 @@ import { RoomRoutes } from '../modules/room/room.route';
 import { CourseRoutes } from '../modules/course/course.route';
 import { SemesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.route';
 import { OfferedCourseRoutes } from '../modules/offeredCourse/offeredCourse.route';
+import { OfferedCourseSectionRoutes } from '../modules/offeredCourseSection/offeredCourseSection.route';
 
 const router = express.Router();
 
@@ -52,6 +53,10 @@ const moduleRoutes = [
   {
     path: '/offered-courses',
     routes: OfferedCourseRoutes,
+  },
+  {
+    path: '/offered-course-sections',
+    routes: OfferedCourseSectionRoutes,
   },
 ];
 
